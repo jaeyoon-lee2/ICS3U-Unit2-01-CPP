29883a12f8fc816acfad55748e139bd1cde85410
Ubuntu <ubuntu@ip-172-31-16-225.ec2.internal>
2019-09-17 01:53:36 +0000

    commit message

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,16 @@
+//Copyright (c) 2019 St. Mother Teresa HS All rights reserved
+
+// Created by: Jaeyoon
+// Created on: Sept 2019
+// This program calculates the area and perimeter of a circle
+//     with radius of 15mm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+	std::cout << "If a circle has a radius of  15mm:" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "Area is " << (M_PI*pow(15.0, 2.0)) << "mm^2" << std::endl;
+	std::cout << "Perimeter is " << (2*M_PI*15) << "mm" << std::endl;
+}
